db320d88fdcfaeaf90c54bb78ab840fee2254e32
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-08-13 15:52:31 +0200

    Target es2022 for top-level await

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2022',
+  },
   plugins: [
     vue(),
     vueJsx(),
